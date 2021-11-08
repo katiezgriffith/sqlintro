72e37958ed56e6a4d670487be1bb5a7d994f408e
Katie Griffith <angel_9586@hotmail.com>
2021-11-08 17:35:03 -0600

    another commit

---
+++ orders.sql
@@ -0,0 +1,13 @@
+CREATE TABLE orders(
+    order_id SERIAL PRIMARY KEY,
+    person_id,
+    product_id,
+    product_name,
+    product_price,
+    quantity
+
+)
+
+INSERT INTO orders(order_id,person_id,product_id,product_name,product_price)
+VALUES
+()
